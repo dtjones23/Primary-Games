5e33a6eea28371d154a5b2476ae0f8f3dfd1dac1
Cyrus212 <135377546+Cyrus212@users.noreply.github.com>
2023-12-18 19:04:12 -0600

    Modified Developer model

--- models/Developer.js
+++ models/Developer.js
@@ -7,21 +7,25 @@ class Developer extends Model {}
 Developer.init(
     // Used to initialize the model with attributes and configuration
     {
+        // Developer's ID
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
         },
+        // Developer's name
         name: {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        // Developer's unique
         slug: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
         },
+        // Developer's games
         games: {
             type: DataTypes.STRING,
             allowNull: false,
