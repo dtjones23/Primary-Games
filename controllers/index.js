87ca39afdbe269e268f35ad3dfbca9a989edeec7
DeAngelo Jones <deangelojones0708@gmail.com>
2023-12-18 21:10:47 -0600

    added login/logout files

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 // Setting up main router
 const router = require('express').Router();
 const apiRoutes = require('./api')
+const userRoutes = require('./userRoutes');
 
 router.use('/api', apiRoutes)
+router.use('/users', userRoutes);
 module.exports = router;
