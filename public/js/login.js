771e54a8d1168af4f936866d2dbb3603fa9c0543
DeAngelo Jones <deangelojones0708@gmail.com>
2023-12-20 16:58:22 -0600

    rough-templates created for registration and login

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,28 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password)
+    try {
+      const response = await fetch('/api/user/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
+      const data = await response.json();
+
       if (response.ok) {
-        document.location.replace('/');
+        window.location.replace('/'); // Redirects to homepage
       } else {
         alert('Failed to log in');
       }
+    } catch (error) {
+      console.error('Error during login:', error);
     }
-  };
-  
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+}
+
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+
