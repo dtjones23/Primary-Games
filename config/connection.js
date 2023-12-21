d1ac31cd3ca11f5c9aaa49bcd64bfef6add22e71
Cyrus212 <135377546+Cyrus212@users.noreply.github.com>
2023-12-20 19:41:05 -0600

    Seeds fix and Checkout page pulling game image

--- config/connection.js
+++ config/connection.js
@@ -8,13 +8,13 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
+        'game_db',
+        'root',
+        '7#06Qq^FRwD^k@b=;JXk{M;Q?',
         {
             host: 'localhost',
             dialect: 'mysql',
-            port: 3001
+            port: 3306
         }
     );
 }
